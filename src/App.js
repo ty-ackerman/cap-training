a24494da840b34e3c7335d781835b10dc155833e
Ackerman <s3095030@bns.bns>
2018-12-10 14:30:31 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 import Dropdown from "./Dropdown";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Dropdown Menu</h1>
         <Dropdown />
       </div>
     );
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,87 +1,93 @@
-// TODOS:
-// 1. Read the ref related code again, understand it.
-// 2. Enchance this dd func a little : if i select "bulbasaur", the plceholder should be replaced with the chosen value and the dd-closes (tricky!!!)
-// 3. accessibility : keyboard ( TAB -> FOCUS, ARROW KEY NAVIAGTION, ESC -> Closes, ENTER-> Selects and does step 2)
-// 4. CSS -> outline shadow, transperant, hover an item, change its color, when the dd is open, the border changes color to show its open
-// 5. gist/github repo for debugging/helps
+import React, {Component} from "react"
+import "./dropdown_styles.css"
 
-import React, { Component } from "react";
-import "./dropdown_styles.css";
-const downArrowImage = require("./down-arrow.png");
-
+const arrow = require("./down-arrow.png")
 const ddItems = [
-  { id: 1, name: "Bulbasaur", level: "grass" },
-  { id: 2, name: "Squirtle", level: "water" },
-  { id: 3, name: "Charmander", level: "fire" }
-];
+    { id: 1, name: "Bulbasaur", level: "grass" },
+    { id: 2, name: "Squirtle", level: "water" },
+    { id: 3, name: "Charmander", level: "fire" }
+  ];
 
 class Dropdown extends Component {
-  constructor() {
-    super();
-    this.state = {
-      displayList: false
-    };
-  }
-
-  //   showDropdown = () => {
-  //     this.setState({ displayList: !this.state.displayList });
-  //   };
-
-  showDropdown = () => {
-    this.setState({ displayList: true });
-    // when i click on the button and the dropdown is open : add a event listener that keeps track of the close fn => when to invoke the close fn?
-    document.addEventListener("click", this.hideDropdown);
-  };
-
-  hideDropdown = event => {
-    if (!this.dropdownRef.contains(event.target)) {
-      this.setState({ displayList: false });
+    constructor() {
+        super()
+        this.state =  {
+            displayDD: false,
+            selectedText: "Select a Character"
+        }
     }
-    document.removeEventListener("click", this.hideDropdown);
-  };
 
-  showDropdownItems = () => {
-    // this.state.displayList ? <render something> : null
-    // <div id="my_id" />
-    // document.getElementById('my_id').currentValue('something')
-    if (this.state.displayList) {
-      return (
-        <ul className="list-container" ref={node => (this.dropdownRef = node)}>
-          {ddItems.map(item => {
+    openDD = () => {
+        this.setState({displayDD: true})
+        //When clicked, add an event listener that keeps track of the close function
+        document.addEventListener("click", this.closeDD)
+        //Problem = dow the entire document has this closeDD function, therefore we are unable to open the dd again.
+        //Solution = check closeDD to REMOVE the closeDD
+    }
+
+
+    closeDD = event => {
+        //event.target = whatever node has been clicked
+        //here we are checking to see if anything EXCEPT the actual li has been clicked
+        //we are doing this by seeing if the node item being clicked matches what is stored in this.dropdownRef
+        if (!this.dropdownRef.contains(event.target)) {
+            //If it doesn't match (i.e. anything other than the li was clicked), displayList should close
+           this.setState({displayDD: false})
+           document.removeEventListener("click", this.closeDD)
+        }
+    }
+
+    getText = event => {
+        this.setState({selectedText: event.target.textContent})
+    }
+
+    changeClass = (nodeText, selected) => {
+        if (nodeText === selected) {
+            return "dd-item selected"
+        }
+        return "dd-item"
+
+    }
+
+    renderItems = () => {
+        if (this.state.displayDD) {
             return (
-              <li className="list-item" key={item.id}>
-                {item.name}
-              </li>
-            );
-          })}
-        </ul>
-      );
-    } else {
-      return null;
+                //This next ref is adding a node to dropdownRef key/value pairing within this (scope is the dropdown class so it's accessible in other functions)
+                <ul ref = {node =>{ 
+                    this.dropdownRef = node}}>
+                    {ddItems.map(item => {
+                        return (
+                        <li 
+                            onClick={this.getText} 
+                            ref = {
+                                node => (node ? (
+                                    node.className = this.changeClass(node.textContent, this.state.selectedText)) : null)
+                            }  
+                            key={item.id}>
+                                {item.name}
+                    </li>
+                    )
+                    })}
+                </ul>
+            )
+        }   
     }
-  };
 
-  render() {
-    // on clicking outside the dropdown, it closes! Anywhere inside it, it stays open
-    // refs are used to identify DOM nodes => if my ref can identify the area of the dd items div
-    const { displayList } = this.state;
-    const iconState = displayList ? "dd-icon-open" : "dd-icon-closed";
-    return (
-      <div>
-        <div className="dd-wrapper" onClick={this.showDropdown}>
-          <div className="title">Select a Character</div>
-          <div className="image">
-            <img
-              className={iconState}
-              src={downArrowImage}
-              alt="toggle arrow"
-            />
-          </div>
-        </div>
-        {this.showDropdownItems()}
-      </div>
-    );
-  }
+    render() {
+        const {displayDD, selectedText} = this.state
+        const iconState = displayDD ? "dd-open" : "dd-closed"
+        return (
+            <div className = "wrapper">
+                <div className={`dd-container ${iconState}`} onClick={this.openDD}>
+                    <div className="select">{selectedText}</div>
+                    <div className="image-container">
+                        <img className={iconState} src={arrow} alt="toggle arrow"/>
+                    </div>
+                </div>
+                {this.renderItems()}
+            </div>
+        )
+    }
 }
 
-export default Dropdown;
+export default Dropdown
